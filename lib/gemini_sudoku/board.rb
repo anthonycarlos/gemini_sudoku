6899d00a3df63838e8b4202dcc1d7c31c1f80396
Chandu Tennety <chandu.tennety@janova.us>
2011-12-29 22:34:54 -0500

    spec out Board constructor

--- lib/gemini_sudoku/board.rb
+++ lib/gemini_sudoku/board.rb
@@ -1,3 +1,8 @@
 class Board
+  attr_reader :cells
+
+  def initialize(side = 9)
+    @cells = Array.new(side, Array.new(side))
+  end
 
 end
--- spec/gemini_sudoku/board_spec.rb
+++ spec/gemini_sudoku/board_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 describe Board do
+
+  it "contains cells" do
+    Board.new.cells.should_not be_empty
+  end
+
   describe "#new" do
     context "with no parameters" do
-      it "creates a square board 9 cells on a side"
+      it "creates a square board 9 cells on a side" do
+        cells = Board.new.cells
+        cells.length.should == 9
+        cells.each{|row| row.length.should == 9}
+      end
     end
   end
 end
